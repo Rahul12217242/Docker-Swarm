1d737db06ed72533d99bed96fe193f5b93207806
Rahul Roy <aliferousrahul@gmail.com>
2025-07-15 09:42:25 +0530

    Create frontend-src-app.js

---
+++ frontend-src-app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Boo from React Frontend! 🌸</h1>
+      <p>Backend is on port 5000, Mongo inside Docker Swarm. </p>
+    </div>
+  );
+}
+
+export default App;
